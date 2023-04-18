524c2e98f95fc7e44271409e677156e83ad6979c
anashamimi <anashamimi2@gmail.com>
2023-04-18 19:50:14 +0200

    add store.js and component cardlist

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
-
+    <CardListComponent/>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import { store } from './data/store';
+import axios from 'axios';
+import CardListComponent from './components/CardListComponent.vue';
   export default {
     name: 'App',
+    components: {
+      CardListComponent,
+    },
     data() {
       return {
-
+        store,
       }
     },
     methods: {
-      
+      getCards() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res) => {
+          console.log(res.data.data);
+          store.cardList = res.data.data;
+        })
+      }
+    },
+    mounted() {
+      this.getCards();
     }
   }
 </script>
---
+++ src/components/CardListComponent.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="(card, index) in store.cardList">
+                {{ card.name }}
+            </div>
+        </div>
+   </div>
+</template>
+
+<script>
+import { store } from '../data/store.js';
+    export default {
+        name: 'CardListComponent',
+        components: {
+
+        }, 
+        data() {
+            return {
+                store
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
