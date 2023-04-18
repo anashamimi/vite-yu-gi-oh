71a421aa29709ab0f2dd25c48961b7a7b783a4dd
anashamimi <anashamimi2@gmail.com>
2023-04-18 18:30:59 +0200

    add vue sass

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+  export default {
+    name: 'App',
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
